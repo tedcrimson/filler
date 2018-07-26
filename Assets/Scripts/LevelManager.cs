4c91302fe17a79cb996b2524054cab117afd84b2
Lasha <lllllaaaassshha@yahoo.com>
2018-07-26 17:48:54 +0400

    random generator

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,30 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : MonoBehaviour
+{
+    [Range(22, 100)]
+    public int levelCoefficient;
+    public GameObject prefab;
+    public Transform spawner;
+    public GameObject MainObject;
 
-	public GameObject prefab;
-	public Transform spawner;
-	public GameObject MainObject;
 
-	// Use this for initialization
-	void Start () {
-		var startPos = spawner.position;
+    void Start()
+    {
+        int randomAngleSum = 0;
+        var startPos = spawner.position;
+        while (randomAngleSum < 340)
+        {
+            var randomAngle = Random.Range(22, levelCoefficient);
+            randomAngleSum += randomAngle;
+            Instantiate(prefab, spawner.position, spawner.rotation, MainObject.transform);
 
-		for (int i = 0; i < 10; i++)
-		{
-			spawner.RotateAround(Vector3.zero, Vector3.forward, 30);
-			Instantiate(prefab, spawner.position, spawner.rotation, MainObject.transform);
-		}
-		spawner.position = startPos;
-		spawner.rotation = Quaternion.identity;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		MainObject.transform.Rotate(Vector3.forward, 1);
+            spawner.RotateAround(Vector3.zero, Vector3.forward, randomAngle);
+        }
+        spawner.position = startPos;
+        spawner.rotation = Quaternion.identity;
+    }
 
-		if(Input.GetMouseButtonDown(0))
-			Debug.Log(MainObject.transform.eulerAngles.z);
-	}
+    void Update()
+    {
+        MainObject.transform.Rotate(Vector3.forward, 1);
+    }
 }
