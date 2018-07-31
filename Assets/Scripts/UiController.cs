ea906294b224bda701acf20482bd13245edb6808
Luka <lukacho_g@yahoo.com>
2018-07-31 09:43:33 +0400

    audio + ui

---
+++ Assets/Scripts/UiController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UiController : MonoBehaviour {
+
+	public Text HitInfo;
+
+	
+	// Update is called once per frame
+	public void UpdateHitInfo (State state) {
+
+		HitInfo.text = state.ToString();
+		
+	}
+}
